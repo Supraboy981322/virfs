f8c87fd5d6aa9c57514d29ffddd694e896613e45
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-03-04 16:57:58 -0600

    test unix

--- test/test.go
+++ test/test.go
@@ -5,11 +5,28 @@ import (
 	"github.com/Supraboy981322/virfs"
 )
 
-var fs = virfs.Init()
 
 func foo() { fmt.Print() }
 
 func main() {
+	unix_test()
+}
+
+func unix_test() {
+	var fs = virfs.Init_UNIX()
+	fmt.Printf("\n%#v\n", fs.Root.Content)
+	if e := fs.Mkdir("/usr"); e != nil && e != virfs.FileExists {
+		panic(e)
+	}
+	fmt.Printf("\n%#v\n", fs.Root.Content["usr"])
+	if e := fs.MkFile("/usr/foo", []byte("bar")); e != nil {
+		panic(e)
+	}
+	fmt.Printf("\n%#v\n", fs.Root.Content["usr"].Dir.Content["foo"].File)
+}
+
+func empty_fs() {
+	var fs = virfs.Init()
 	if e := fs.Mkdir("/usr"); e != nil {
 		panic(e)
 	}
@@ -17,5 +34,5 @@ func main() {
 	if e := fs.MkFile("/usr/foo", []byte("bar")); e != nil {
 		panic(e)
 	}
-	fmt.Printf("\n%#v\n", fs.Root.Content["usr"].Dir.Content["foo"].File.Content)
+	fmt.Printf("\n%#v\n", fs.Root.Content["usr"].Dir.Content["foo"].File)
 }
