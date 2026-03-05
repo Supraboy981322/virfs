601d75a3f93d39898adf19625897b466484b8453
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-03-04 19:45:37 -0600

    comments ; formatting

--- helpers.go
+++ helpers.go
@@ -5,52 +5,96 @@ import (
 	"fmt"
 	"path/filepath"
 )
+//so I can just willy-nilly insert a print statement without importing again 
 func _(){fmt.Print()}
 
+//helper to validate path
 func Is_valid_path(path string) bool {
 	var e error
+
+	//cleanup path
 	foo := filepath.Clean(path)
+
+	//make sure it's absolute
 	foo, e = filepath.Abs(path)
 	if e != nil { return false }
+
+	//ensure it's in UNIX format 
 	foo = filepath.ToSlash(foo)
+
+	//just to be absolutely sure
 	return foo[0] == '/'
 }
 
+//get the dir of a path
 func Get_basepath(path string) (string, error) {
+	//resolve to absolute path 
 	p, e := Resolve_path(path)
 	if e != nil { return "", e }
-	return filepath.Dir(p), nil
+
+	//get directory
+	d := filepath.Dir(p)
+
+	//be 100% sure it's in UNIX format
+	d = filepath.ToSlash(d)
+	if d[0] != '/' { panic("FAILED TO FORCE UNIX DIR") } 
+
+	//return the directory
+	return d, nil
 }
 
+//helper to get the name of entry from path 
 func Get_name(path string) string {
+	//resolve to abs
 	p, _ := Resolve_path(path)
-	//split := strings.Split(p, "/") 
-	return filepath.Base(p) //split[len(split)-1]
+
+	//filepath package is good enough for this
+	return filepath.Base(p)
 }
 
+//internal helper to traverse to a path 
 func (fs Fs) goto_path(path string) (*Dir, error) {
+	//validate path
 	if !Is_valid_path(path) { return nil, InvalidPath }
 
+	//resolve to abs
 	p, e := Resolve_path(path)
 	if e != nil { return nil, e }
 
+	//get the parent dir
 	base, e := Get_basepath(p)
-	if len(base) == 0 { return &fs.Root, nil }
 	if e != nil { return nil, e }
+	//if parent name is empty, assume it's the root dir and return it
+	if len(base) == 0 { return &fs.Root, nil }
+
+	//remove the leading forward slash (for splitting) 
 	if base[0] == '/' { base = base[1:] }
+	
+	//split the path
 	path_split := strings.Split(base, "/")
+
+	//if the first entry is empty string, shift the slice
 	if len(path_split[0]) == 0 {
 		path_split = path_split[1:]
 	}
 
+	//start at fs root
 	current := &(fs.Root)
+	//iterate over split path
 	for _, d := range path_split {
+
+		//err if target entry isn't present in current dir
 		if !current.Contains(d) { return nil, DirNotExist }
+
+		//move current dir to the next entry in path
 		current = (*current).Content[d].Dir
 	}
+
+	//return pointer to last dir traversed into 
 	return current, nil
 }
 
+//helper to check if a dir contains an entry name
 func (d Dir) Contains(name string) bool {
 	for n := range d.Content {
 		if name == n { return true }
@@ -58,17 +102,27 @@ func (d Dir) Contains(name string) bool {
 	return false
 }
 
+//helper to check if a path is root dir
 func (fs Fs) Is_root(path string) bool {
 	p, _ := Resolve_path(path)
 	return p == filepath.Dir(p)
 }
 
+//helper to resolve a path to valid (hopefully), abs path
 func Resolve_path(path string) (string, error) {
 	var e error
+
+	//cleanup relative traversal
 	foo := filepath.Clean(path)
+
+	//ensure it's resolved to abs
 	foo, e = filepath.Abs(path)
 	if e != nil { return "", e }
+
+	//be absolutely sure ensure it's in UNIX format
 	foo = filepath.ToSlash(foo)
 	if foo[0] != '/' { return "", InvalidPath }
+
+	//return resulting path
 	return foo, nil
 }
--- main.go
+++ main.go
@@ -5,7 +5,62 @@ import (
 	"errors"
 )
 
-type Entry_type int
+//so I can just willy-nilly insert a print statement without importing again 
+func _(){fmt.Print()}
+
+//types
+type (
+	//why no dedicated enum type?
+	Entry_type int
+
+	//directory
+	Dir struct {
+		Content map[string]Entry
+		Name string
+		//points to self if root dir
+		Parent *Dir
+
+		// TODO:
+		//  - properties
+		//  - permissions
+		//  - size
+		//  - probably some other stuff
+	}
+
+	//file
+	File struct {
+		Content []byte
+
+		// TODO:
+		//  - properties
+		//  - permissions
+		//  - size
+		//  - probably some other stuff
+	}
+
+	//
+	Entry struct {
+		Entry_type Entry_type
+		//name of file/dir (not path)
+		Name string
+		//if file, will be nil
+		Dir *Dir
+		//if dir, will be nil
+		File *File
+	}
+
+	Fs struct {
+		//root dir ('/')
+		Root Dir
+		// TODO:
+		//  - properties
+		//  - settings
+		//  - size
+		//  - probably some other stuff
+	}
+)
+
+//why no dedicated enum type?
 const (
 	File_entry Entry_type = iota
 	Dir_entry
@@ -14,29 +69,7 @@ const (
 	Socket_entry
 )
 
-type (
-	Dir struct {
-		Content map[string]Entry
-		Name string
-		Parent *Dir
-	}
-
-	File struct {
-		Content []byte
-	}
-
-	Entry struct {
-		Entry_type Entry_type
-		Dir *Dir //if file, will be nil
-		File *File //if dir, will be nil
-		Name string
-	}
-)
-
-type Fs struct {
-	Root Dir
-}
-
+//errors as values sure is a great idea
 var (
 	EmptyPath = errors.New("cannot use empty path")
 	DirNotExist = errors.New("path does not exist")
@@ -47,6 +80,7 @@ var (
 	FileNotExist = errors.New("file does not exist")
 )
 
+//initialize empty fs
 func Init() Fs {
 	fs := Fs {
 		Root: Dir {
@@ -54,28 +88,41 @@ func Init() Fs {
 			Name: "/",
 		},
 	}
+
+	//make root parent point to itself
 	fs.Root.Parent = &fs.Root
+
 	return fs
 }
 
+//initialize with UNIX dirs
 func Init_UNIX() Fs {
 	fs := Fs {
 		Root: Unix_root_dir(),
 	}
+
+	//make root parent point to itself
 	fs.Root.Parent = &fs.Root
+
 	return fs
 }
 
+//make dir (takes absolute path only)
 func (fs Fs) Mkdir(path string) error {
+	//basic validity checks 
 	if len(path) < 1 { return EmptyPath }
 	if path[0] != '/' { return InvalidPath }
 
+	//attempt to traverse to the parent of input path
 	current, e := fs.goto_path(path)
 	if e != nil { return e } 
 
+	//get the name of the dir
 	target := Get_name(path)
+	//if already exists or is root, err
 	if current.Contains(target) || fs.Is_root(path) { return FileExists }
 
+	//add the entry to the path's parent dir 
 	(*current).Content[target] = Entry {
 		Entry_type: Dir_entry,
 		Dir: &Dir{
@@ -89,41 +136,89 @@ func (fs Fs) Mkdir(path string) error {
 	return nil
 }
 
+//make file (takes absolute path only and content)
 func (fs Fs) MkFile(path string, content []byte) error {
+	//validate path
 	if !Is_valid_path(path) { return InvalidPath }
 
+	//traverse to path's parent
 	current, e := fs.goto_path(path)
 	if e != nil { return e }
 
+	//get the name of the file
 	target := Get_name(path)
+	//make sure the file isn't already present and isn't root dir
 	if current.Contains(target) || fs.Is_root(path) { return FileExists }
+	//make sure the target isn't empty (can occur if trailing slash in path)
 	if len(target) == 0 { return InvalidPath }
 
-	file := File{
-		Content: content,
-	}
-
+	//create the entry
 	(*current).Content[target] = Entry { 
 		Entry_type: File_entry,
 		Dir: nil,
-		File: &file,
+		File: &File{
+			Content: content,
+		},
 		Name: target, 
 	}
 	
 	return nil
 }
 
-//so I can just willy-nilly insert a print statement without importing again 
-func _(){fmt.Print()}
+//remove a dir (takes absolute path only, set 'force' to true to
+//  delete non-empty dir)
+func (fs Fs) RmDir(path string, force bool) error {
+	//traverse to the path's parent dir
+	p, e := fs.goto_path(path)
+	if e != nil { return e }
 
+	//get the dir name
+	name := Get_name(path)
+
+	//make sure it's present
+	if !p.Contains(name) { return DirNotExist }
+
+	//err if not a dir
+	if p.Content[name].Entry_type != Dir_entry { return Type_mismatch }
+
+	//make sure the directory isn't empty if no force
+	if !force && len(p.Content[name].Dir.Content) != 0 { return DirNotEmpty }
+
+	//remove from fs
+	delete(p.Content, name)
+
+	return nil
+}
+
+//delete a file (takes absolute path only, set 'recurse' to true
+//  if path may be dir)
+func (fs Fs) RmFile(path string, recurse bool) error {
+	p, e := fs.goto_path(path)
+	if e != nil { return e }
+	name := Get_name(path)
+	if !p.Contains(name) { return FileNotExist }
+	if p.Content[name].Entry_type != File_entry {
+		if recurse { return fs.RmDir(path, true) }
+		return Type_mismatch
+	}
+	delete(p.Content, name)
+	return nil
+}
+
+//returns dir that mimiks basic UNIX root dir
 func Unix_root_dir() Dir {
+	//empty root dir
 	var root = Dir {
 		Content: map[string]Entry{},
 		Name: "/",
 	}
+
+  //the most basics of basic ('/home' was a later addition to Unix)
 	paths := []string {
 		"usr", "etc", "var", "bin", "lib", "media", "mnt", "tmp",
 	}
+
+	//add each dir to fs root
 	for _, p := range paths {
 		root.Content[p] = Entry {
 			Entry_type: Dir_entry,
@@ -136,30 +231,6 @@ func Unix_root_dir() Dir {
 			Name: p,
 		}
 	}
+
 	return root
-}
-
-func (fs Fs) RmDir(path string, force bool) error {
-	p, e := fs.goto_path(path)
-	if e != nil { return e }
-	name := Get_name(path)
-	if !p.Contains(name) { return DirNotExist }
-	if p.Content[name].Entry_type != Dir_entry { return Type_mismatch }
-
-	if len(p.Content[name].Dir.Content) != 0 && !force { return DirNotEmpty }
-	delete(p.Content, name)
-	return nil
-}
-
-func (fs Fs) RmFile(path string, recurse bool) error {
-	p, e := fs.goto_path(path)
-	if e != nil { return e }
-	name := Get_name(path)
-	if !p.Contains(name) { return FileNotExist }
-	if p.Content[name].Entry_type != File_entry {
-		if recurse { return fs.RmDir(path, true) }
-		return Type_mismatch
-	}
-	delete(p.Content, name)
-	return nil
 }
