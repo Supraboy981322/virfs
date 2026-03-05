1783cfbc206dcebd625fd41e3f3148f2cf6946ea
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-03-04 19:46:12 -0600

    formatting ; reorganize

--- test/main.go
+++ test/main.go
@@ -1,1 +1,13 @@
 package main
+
+import ("fmt")
+
+func passed(msg string, a ...any) {
+	fmt.Printf("[\x1b[32mGOOD\x1b[0m]:  " + msg + "\n", a...)
+}
+func task(msg string, a ...any) {
+	fmt.Printf("\n[\x1b[36mtask\x1b[0m]:  " + msg + "\n", a...)
+}
+func failed(msg string, a ...any) {
+	fmt.Printf("[\x1b[31;1mFAILED\x1b[0m]:  " + msg + "\n", a...)
+}
--- test/test.go
+++ test/test.go
@@ -1,12 +1,13 @@
+// TODO: *more* thorough testing
+
 package main
 
 import (
 	"fmt"
 	"github.com/Supraboy981322/virfs"
 )
-
-
-func foo() { fmt.Print() }
+//so I can just willy-nilly insert a print statement without importing again 
+func _(){fmt.Print()}
 
 func main() {
 	unix_test()
@@ -21,59 +22,79 @@ func unix_test() {
 	e = fs.Mkdir("/usr")
 	if e != virfs.FileExists {
 		failed("%v", e)
-	} else { passed("file already exists") }
+	} else {
+		passed("file already exists")
+	}
 
 	task("create file /usr/foo")
 	e = fs.MkFile("/usr/foo", []byte("bar"))
 	if e != nil && fs.Root.Content["usr"].Dir.Contains("foo") {
 		failed("%v", e)
-	} else { passed("created file in subdir to root") }
+	} else {
+		passed("created file in subdir to root")
+}
 
 	task("verify /usr/foo contents")
 	if string(fs.Root.Content["usr"].Dir.Content["foo"].File.Content) != "bar" {
 		failed("file contents did not match")
-	} else { passed("file contents matched") }
+	} else {
+		passed("file contents matched")
+	}
 
 	task("attempt to replace root dir with file")
 	e = fs.MkFile("/", []byte("foo"))
 	if e != virfs.FileExists && !fs.Root.Contains("/") && !fs.Root.Contains("") {
 		failed("successfully (bad) repaced root dir with file (err: %v)", e)
-	} else { passed("prevented replacing root dir with file") }
+	} else {
+		passed("prevented replacing root dir with file")
+	}
 
 	task("make file /bar")
 	e = fs.MkFile("/bar", []byte("foo"))
 	if e != nil && fs.Root.Contains("bar") {
 		failed("%v", e)
-	} else { passed("created file in root dir") }
+	} else {
+		passed("created file in root dir")
+	}
 
 	task("verify /bar contents")
 	if string(fs.Root.Content["bar"].File.Content) != "foo" {
 		failed("file contents did not match")
-	} else { passed("file contents matched") }
+	} else {
+		passed("file contents matched")
+	}
 
 	task("attempt to create file that already exists (/bar)")
 	e = fs.MkFile("/bar", []byte("bar"))
 	if e == nil || !fs.Root.Contains("bar") {
 		failed("successfully (bad) created file that should already exist")
-	} else { passed("disallowed created file that already exists") }
+	} else {
+		passed("disallowed created file that already exists")
+	}
 	
 	task("create dir /usr/keeper")
 	e = fs.Mkdir("/usr/keeper")
 	if e != nil || !fs.Root.Content["usr"].Dir.Contains("keeper") {
 		failed("failed to  that should already exist")
-	} else { passed("disallowed created file that already exists") }
+	} else {
+		passed("disallowed created file that already exists")
+	}
 
 	task("delete /bar")
 	e = fs.RmFile("/bar", false)
 	if e != nil {
 		failed("failed to remove file from root dir (/bar)")
-	} else { passed("removed file file root dir") }
+	} else {
+		passed("removed file file root dir")
+	}
 
 	task("delete /usr/keeper")
 	e = fs.RmDir("/usr/keeper", false)
 	if e != nil {
 		failed("failed to delete dir")
-	} else { passed("removed dir") }
+	} else {
+		passed("removed dir")
+	}
 
 	if len(fs.Root.Content["usr"].Dir.Content) < 1 {
 		panic("rearange tests, expected the /usr dir to not be empty but was")
@@ -82,7 +103,9 @@ func unix_test() {
 	e = fs.RmDir("/usr", false)
 	if e == nil {
 		failed("seccessfully (bad) deleted non-empty dir that has contents")
-	} else { passed("prevented removal of non-empty dir") }
+	} else {
+		passed("prevented removal of non-empty dir")
+	}
 }
 
 func empty_fs() {
@@ -95,14 +118,4 @@ func empty_fs() {
 		panic(e)
 	}
 	fmt.Printf("\n%#v\n", fs.Root.Content["usr"].Dir.Content["foo"].File)
-}
-
-func passed(msg string, a ...any) {
-	fmt.Printf("[\x1b[32mGOOD\x1b[0m]:  " + msg + "\n", a...)
-}
-func task(msg string, a ...any) {
-	fmt.Printf("\n[\x1b[36mtask\x1b[0m]:  " + msg + "\n", a...)
-}
-func failed(msg string, a ...any) {
-	fmt.Printf("[\x1b[31;1mFAILED\x1b[0m]:  " + msg + "\n", a...)
 }
