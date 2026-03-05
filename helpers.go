9e039813762920d2cd2f353b0ae8109e4c027427
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-03-05 10:31:25 -0600

    chk dir struct is root

--- helpers.go
+++ helpers.go
@@ -107,6 +107,7 @@ func (fs Fs) Is_path_root(path string) bool {
 	p, _ := Resolve_path(path)
 	return p == filepath.Dir(p)
 }
+
 //helper to check if a path is root dir
 func (fs Fs) Is_root(dir *Dir) bool {
 	return dir == &fs.Root
